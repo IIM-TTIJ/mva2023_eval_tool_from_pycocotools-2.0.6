4d6809f93635e214ffeda9f0e65f175e2c0c16b4
cyoukaikai <40422292+cyoukaikai@users.noreply.github.com>
2022-12-19 15:57:47 +0900

    Add files via upload

--- pycocotools/cocoeval.py
+++ pycocotools/cocoeval.py
@@ -506,13 +506,18 @@ class Params:
         self.catIds = []
         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
         # self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
+        # self.maxDets = [1, 10, 100]
+        # self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+        # self.areaRngLbl = ['all', 'small', 'medium', 'large']
+
         # array([0.25, 0.5, 0.75])
         self.iouThrs = np.linspace(.25, 0.75, int(np.round((0.75 - .25) / .25)) + 1, endpoint=True)
+        self.maxDets = [100]
+        self.areaRng = [[0 ** 2, 1e5 ** 2],]
+        self.areaRngLbl = ['all', ]
 
+        # ?recall thresholds for evaluation, here we do not report recall, so do not modify it.
         self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
-        self.maxDets = [1, 10, 100]
-        self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
-        self.areaRngLbl = ['all', 'small', 'medium', 'large']
         self.useCats = 1
 
     def setKpParams(self):
